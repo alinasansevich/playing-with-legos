6447ca832af20d5e0f558c535ec8abd844614635
Alina Sansevich <alinasansevich@gmail.com>
2020-11-15 14:37:42 -0600

    First commit.

---
+++ exploring_lego_sets.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 15 14:20:31 2020
+
+@author: alina
+
+Resources:
+
+"""
+
+
+import pandas as pd
+import numpy as np
+
+sets = pd.read_csv('sets.csv')
+
+themes = pd.read_csv('themes.csv')
+
+parts = pd.read_csv('parts.csv')
+
+minifigs = pd.read_csv('minifigs.csv')
+
+colors = pd.read_csv('colors.csv')
+
